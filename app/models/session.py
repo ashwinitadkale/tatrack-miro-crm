7ceac2eea989271624d07e390159e1e18c86f766
Ashwini Tadkale <ashwinitadkale@gmail.com>
2026-01-29 22:36:39 +0530

    fix typo: error

--- app/models/session.py
+++ app/models/session.py
@@ -4,17 +4,20 @@ from datetime import datetime
 class Session(db.Model):
     __tablename__="sessions"
 
-    id=db.Column(
+    id=db.Column(db.Integer,primary_key=True)
+     
+
+    inquiry_id=db.Column(
         db.Integer,
         db.ForeignKey("inquiries.id"),
         nullable=False
     )
     session_date=db.Column(db.Date,nullable=False)
-    session_time=db.column(db.String(20))
-    status=db.Column(
-        db.String(20),
-        default="scheduled"
-    )
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    session_time=db.Column(db.String(20),nullable=False)
+    # status=db.Column(
+    #     db.String(20),
+    #     default="scheduled"
+    # )
+    # created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
     inquiry = db.relationship("Inquiry", backref="sessions")
